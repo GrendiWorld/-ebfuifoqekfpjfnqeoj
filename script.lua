8fe4aa84935d4fb62ca17583998bfcd196688c15
GrendiWorld <ila025590@gmail.com>
2025-12-06 23:08:30 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -834,6 +834,20 @@ function lagcompensation:FixPvs(pCurEntity)
 end
 
 
+local air_strafe = ui.reference("Misc", "Movement", "Air strafe")
+local enable_it = ui.new_checkbox("LUA", "A", "FS")
+
+
+client.set_event_callback("setup_command", function(c)
+    if (ui.get(enable_it)) then
+        local vel_x, vel_y = entity.get_prop(entity.get_local_player(), "m_vecVelocity")
+        local vel = math.sqrt(vel_x^2 + vel_y^2)
+        ui.set(air_strafe, not (c.in_jump and (vel < 10)) or ui.is_menu_open())
+    end
+end)
+
+
+
 HellpineC = {
     ClanTag = {
         state = {enabled = false, last_tag = "", frame = 1, last_time = 0},
